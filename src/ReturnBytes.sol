96a2b467ca7173c254223f3145c14b8d48ee8cab
typicalHuman <reallytovald@gmail.com>
2025-02-08 18:42:13 +0500

    fix

--- src/ReturnBytes.sol
+++ src/ReturnBytes.sol
@@ -5,17 +5,15 @@ contract ReturnBytes {
     function main(
         address a,
         uint256 b
-    ) external pure returns (bytes memory result) {
-        // I know it's outside yul border, but I couldn't figure out how to return bytes memory without additional offset + length metadata so it could be compared in the test
-        // because in remix decoded input equals to what we have to calculate, I doubt that we need to make some bytes manipulations to exclude bytes memory metadata from returned result
-        // and also simply returning bytes in correct format will give random buffer_overrun revert, even though returned data is correct - I think something wrong with tx decoder that foundry uses
-        // because in Remix I can send this transaction and I will get this "error" only in decoded message" but output is fine
-        result = new bytes(64);
+    ) external pure returns (bytes memory) {
         assembly {
             // your code here
             // encode a and b `abi.encode(a,b)` and return it.
-            mstore(add(result, 0x20), a)
-            mstore(add(result, 0x40), b)
+            mstore(0x00, 0x20)
+            mstore(0x20, 0x40)
+            mstore(0x40, a)
+            mstore(0x60, b)
+            return(0x00, 0x80)
         }
     }
 }
